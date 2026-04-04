52fa626d6ca3cf991be7a81e2da808b763195893
Yotam Lifschytz <80533728+yotam-lif@users.noreply.github.com>
2026-04-04 23:44:50 +0300

    crap

--- data/gen_data/gen_dat_pspin.sh
+++ data/gen_data/gen_dat_pspin.sh
@@ -11,11 +11,11 @@
 # Load Miniconda module
 module load miniconda/24.9.2_environmentally
 
+## Initialize Conda for this non-interactive shell
+#source "$(conda info --base)/etc/profile.d/conda.sh"
+
 # Activate the Conda environment
 conda activate my_env
 
 # Run the Python script
 python gen_dat_pspin.py --N 40 --P 1 --n_repeats 2
-
-# Deactivate the environment after the script finishes
-conda deactivate
